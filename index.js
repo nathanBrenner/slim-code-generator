81d06bdce1b4b56303488a595bfcdf93c269514a
nathan-j-brenner <nathan.j.brenner@gmail.com>
2018-03-31 17:29:09 -0700

    modified to work as a command line script

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 var prompt = require('prompt');
 var generateFiles = require('./scripts/createSpecFile');
 
@@ -5,5 +6,3 @@ prompt.get(['directory', 'template'], function (err, result) {
 	console.log(`Generating boilerplate code in ${result.directory} directory with the ${result.template} template.`);
 	generateFiles(result.directory, result.template);
 });
-// ./components
-// templates/react-spec-template
