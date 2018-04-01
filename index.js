aa327bf77aa69ac980254544b8a20edf27fc763a
nathan-j-brenner <nathan.j.brenner@gmail.com>
2018-04-01 16:59:39 -0700

    documented and cleaned up the index script

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 #!/usr/bin/env node
-var prompt = require('prompt');
-var generateFiles = require('./scripts/createSpecFile');
+var prompt = require("prompt");
+var generateFiles = require("./scripts/createSpecFile");
 
-prompt.get(['directory', 'template'], function (err, result) {
-	console.log(`Generating boilerplate code in ${result.directory} directory with the ${result.template} template.`);
-	generateFiles(result.directory, result.template);
+prompt.get(["directory", "template"], (err, { directory, template }) => {
+	/**
+	 * @param directory: string;
+	 * 		This should be the path to the directory you want files to be generated on.
+	 * 		This directory path is relative to where you are when you run the 'generator' cli
+	 * 
+	 * @param template: string;
+	 * 		This should be the path to the template file
+	 * 		This path should be absolute to the application
+	 * 		You can use this modules provided templates or create your own
+	 */
+
+  console.log(
+    `Generating boilerplate code in ${directory} directory with the ${template} template.`
+  );
+
+  generateFiles(directory, template);
 });
