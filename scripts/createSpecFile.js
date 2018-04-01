bb66f4100deac7a093e27dd469dd6cd666ee0919
nathan-j-brenner <nathan.j.brenner@gmail.com>
2018-03-31 18:03:27 -0700

    more debugging

--- scripts/createSpecFile.js
+++ scripts/createSpecFile.js
@@ -6,7 +6,7 @@ function createSpecFile({component, path, templatePath}) {
 	const template = require(templatePath);
 	const data = template({component, path});
 	const file = `${path}/${component}.spec.js`;
-
+	console.log({appDirectory, file})
 	fs.writeFile(file, data, err => {
 		if (err) throw err;
 		console.log(`${component} was added to ${path}`);
@@ -18,7 +18,7 @@ module.exports = function generateFiles(directory, templatePath) {
 	const files = fs.readdirSync(directory);
 	const components = files.map(file => file.split('.')).map(file => file[0]);
 	const paths = components.map(component => `./${component}`);
-	
+	console.log('generateFiles', {components, path})
 	components.forEach((component, i) => {
 		createSpecFile({component: components[i], path: paths[i], templatePath})
 	});
