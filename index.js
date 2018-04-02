50a98f4c2533751aa1a7675b66b85429c93d9310
nathan-j-brenner <nathan.j.brenner@gmail.com>
2018-04-02 13:54:50 -0700

    removed template arg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var prompt = require("prompt");
 var generateFiles = require("./scripts/generateFiles");
 
-prompt.get(["directory", "template"], (err, { directory, template }) => {
+prompt.get(["directory"], (err, { directory }) => {
 	/**
 	 * @param directory: string;
 	 * 		This should be the path to the directory you want files to be generated on.
@@ -12,7 +12,12 @@ prompt.get(["directory", "template"], (err, { directory, template }) => {
 	 * 		This should be the path to the template file
 	 * 		This path should be absolute to the application
 	 * 		You can use this modules provided templates or create your own
+	 * 
+	 * todo: switch from prompt to something with command line args with flag
+	 * Switched this to take template directly
 	 */
+
+	const template = '../templates/react-spec-template';
 
   console.log(
     `Generating boilerplate code in ${directory} directory with the ${template} template.`
